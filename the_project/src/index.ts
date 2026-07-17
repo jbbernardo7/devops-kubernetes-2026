da4c09a195b993d393eab993f463089d036421de
João Bernardo <a045900@umaia.pt>
2026-07-17 21:42:39 +0100

    exercise 1.5

--- the_project/src/index.ts
+++ the_project/src/index.ts
@@ -4,6 +4,11 @@ const server = fastify();
 const host = process.env.HOST ?? "0.0.0.0";
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
+server.get("/", async (req, reply) => {
+	reply.type("text/html").send("<h1>Program working correctly  :)</h1>");
+});
+
+
 server.listen({ host, port }, (err) => {
   if (err) {
     console.error(err);
